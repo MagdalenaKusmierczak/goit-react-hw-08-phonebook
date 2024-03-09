795d465796e9fee036fb40c7073eafdc9220a247
Magdalena Kuśmierczak <magdalena-jarek@o2.pl>
2024-03-09 15:30:36 +0100

    navigation with validation

--- src/components/Navigation/Navigation.jsx
+++ src/components/Navigation/Navigation.jsx
@@ -1,12 +1,21 @@
+import { useSelector } from 'react-redux';
+import { selectIsLoggedIn } from '../../redux/authorization/selectors';
+
 import { NavigationWrapper, NavigationItem } from './Navigation.styled';
 
 const Navigation = () => {
+  const isLoggedIn = useSelector(selectIsLoggedIn);
   return (
     <NavigationWrapper>
       <NavigationItem to="/">Home</NavigationItem>
-      <NavigationItem to="/register">Register</NavigationItem>
-      <NavigationItem to="/login">Login</NavigationItem>
-      <NavigationItem to="/contacts">Contacts</NavigationItem>
+      {isLoggedIn ? (
+        <NavigationItem to="/contacts">Contacts</NavigationItem>
+      ) : (
+        <>
+          <NavigationItem to="/register">Register</NavigationItem>
+          <NavigationItem to="/login">Login</NavigationItem>
+        </>
+      )}
     </NavigationWrapper>
   );
 };
